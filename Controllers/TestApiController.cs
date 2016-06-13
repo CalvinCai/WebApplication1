a5478696281d53e5e3ba4c6030e75dec28191d78
CalvinCai <Calvin.Cai@software.dell.com>
2016-06-13 15:40:47 +0800

    fitst

---
+++ Controllers/TestApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class TestApiController : ApiController
+    {
+        public string Test1()
+        {
+            return "Hello world!";
+        }
+
+        public string Test2(string para)
+        {
+
+            return "Hello world! the para is :" + para;
+        }
+    }
+}
